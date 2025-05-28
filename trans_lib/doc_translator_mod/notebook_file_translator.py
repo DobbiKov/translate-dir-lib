b103e40891c0421cc03023f8eeef6d6bb51839a3
Yehor Korotenko <dobbikov@gmail.com>
2025-05-28 18:03:08 +0200

    tried not to translate already translated cells

--- trans_lib/doc_translator_mod/notebook_file_translator.py
+++ trans_lib/doc_translator_mod/notebook_file_translator.py
@@ -1,4 +1,5 @@
 from asyncio import sleep
+import os
 from pathlib import Path
 from ..enums import Language
 from ..helpers import read_string_from_file
@@ -9,21 +10,51 @@ from loguru import logger
 
 async def translate_notebook_async(source_file_path: Path, target_file_path: Path, target_language: Language) -> None:
     nb = jupytext.read(source_file_path)
+    tgt_nb = None
+    if os.path.exists(target_file_path):
+        tgt_nb = jupytext.read(target_file_path)
+    # TODO: read target file
     for i in range(len(nb.cells)):
-        src_txt = nb.cells[i].source
-        cell_type = nb.cells[i].cell_type
-        checksum = hashlib.md5(src_txt.encode()).hexdigest()
-        nb.cells[i].metadata.setdefault("tags", [])
-        nb.cells[i].metadata["tags"].append("needs_review")
-        nb.cells[i].metadata["checksum"] = checksum
-        await sleep(5)
-        match cell_type:
-            case "code":
-                nb.cells[i].source = await translate_code_cell_async(src_txt, target_language)
-                nb.cells[i].metadata
-            case _:
-                nb.cells[i].source = await translate_markdown_cell_async(src_txt, target_language)
+        tgt_cell = None
+        id_to_find = nb.cells[i].get("id")
+        logger.debug(f"Trying to find {id_to_find}")
+        if tgt_nb is not None and id_to_find is not None: # if target notebook exists and the current cell and its id exists
+            for cell in tgt_nb.cells: 
+                print("", cell.get("id"))
+                if cell.get("id") == id_to_find:
+                    logger.debug(f"Found {id_to_find}")
+                    tgt_cell = cell
+                    break
+        # WARNING: temp none, change
+        nb.cells[i] = await translate_jupyter_cell_async(nb.cells[i], tgt_cell, target_language) 
     jupytext.write(nb, target_file_path)
+
+async def translate_jupyter_cell_async(cell: dict, tgt_cell: dict | None, target_language: Language) -> dict:
+    src_txt = cell["source"]
+    cell_type = cell["cell_type"]
+    checksum = hashlib.md5(src_txt.encode()).hexdigest()
+
+    if tgt_cell is not None:
+        temp_metadata = tgt_cell["metadata"] or {}
+        tgt_checksum = temp_metadata.get("checksum")
+        if tgt_checksum is not None:
+            if tgt_checksum == checksum: # if checksums are equals, then the translated 
+                logger.debug("this one is translated, won't translate this one")
+                return cell
+        
+    
+    logger.debug("this one is not translated, start translation")
+    cell["metadata"].setdefault("tags", [])
+    cell["metadata"]["tags"].append("needs_review")
+    cell["metadata"]["checksum"] = checksum
+    await sleep(5)
+    match cell_type:
+        case "code":
+            cell["source"] = await translate_code_cell_async(src_txt, target_language)
+        case _:
+            cell["source"] = await translate_markdown_cell_async(src_txt, target_language)
+
+    return cell
 
 MARKDOWN_PROMPT_PATH = Path("/Users/dobbikov/Desktop/stage/prompts/doc_specific/markdown_prompt")
 CODE_PROMPT_PATH = Path("/Users/dobbikov/Desktop/stage/prompts/doc_specific/code_prompt")
@@ -55,7 +86,6 @@ async def translate_markdown_cell_async(contents: str, target_language: Language
 async def translate_code_cell_async(contents: str, target_language: Language) -> str:
     prompt = get_code_prompt_text()
     prompt = _prepare_prompt_for_language(prompt, target_language)
-    logger.debug(f"prompt: {prompt}")
     return await translate_chunk_with_prompt(prompt, contents)
 
 
