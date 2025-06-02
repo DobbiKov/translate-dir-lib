e38be7c92ecad0c29d8d9ed3b923fc9f67c05545
Yehor Korotenko <dobbikov@gmail.com>
2025-06-02 11:49:16 +0200

    changed to verify correspondence by contents

--- trans_lib/translator_corrector.py
+++ trans_lib/translator_corrector.py
@@ -3,8 +3,8 @@
 from pathlib import Path
 from trans_lib.enums import Language
 from trans_lib.errors import ChecksumNotFoundError
-from trans_lib.helpers import calculate_checksum
-from trans_lib.trans_db import add_contents_to_db, find_correspondent_checksum, read_contents_by_checksum_with_lang, set_checksum_pair_to_correspondence_db
+from trans_lib.trans_db import add_contents_to_db, do_translation_correspond_to_source, read_contents_by_checksum_with_lang, set_checksum_pair_to_correspondence_db
+
 
 
 def correct_chunk_translation(root_path: Path, src_checksum: str, src_lang: Language, new_translation: str, tgt_lang: Language) -> None:
@@ -15,12 +15,9 @@ def correct_chunk_translation(root_path: Path, src_checksum: str, src_lang: Lang
     if _src_contents is None:
         raise ChecksumNotFoundError
 
-    tgt_checksum = add_contents_to_db(root_path, new_translation, tgt_lang)
-
-    init_tgt_checksum = find_correspondent_checksum(root_path, src_checksum, src_lang, tgt_lang)
-
-    if tgt_checksum == init_tgt_checksum:
+    if do_translation_correspond_to_source(root_path, src_checksum, src_lang, new_translation, tgt_lang):
         return 
 
+    tgt_checksum = add_contents_to_db(root_path, new_translation, tgt_lang) 
     set_checksum_pair_to_correspondence_db(root_path, src_checksum, src_lang, tgt_checksum, tgt_lang)
     
