4c8cd48c8941c4ad3f79d17ccfdfe2f66cc93cf2
Yehor Korotenko <dobbikov@gmail.com>
2025-06-10 16:43:14 +0200

    fixed the trhansalte file function logic

--- trans_lib/doc_translator.py
+++ trans_lib/doc_translator.py
@@ -1,4 +1,6 @@
 from loguru import logger
+
+from trans_lib.vocab_list import VocabList
 from .enums import DocumentType, Language
 from .translator import translate_contents_async
 from .helpers import read_string_from_file, analyze_document_type
@@ -9,11 +11,8 @@ from .doc_translator_mod import latex_file_translator
 
 async def translate_file_async(source_path: Path, target_language: Language) -> str:
     """Reads a file, translates its content asynchronously, and returns the translated content."""
-    doc_type = analyze_document_type(source_path)
-    match doc_type:
-        case _:
-            file_contents = read_string_from_file(source_path)
-            return await translate_contents_async(file_contents, target_language)
+    file_contents = read_string_from_file(source_path)
+    return await translate_contents_async(file_contents, target_language)
 
 
 async def translate_file_to_file_async(
@@ -21,7 +20,8 @@ async def translate_file_to_file_async(
     source_path: Path,
     source_language: Language,
     target_path: Path,
-    target_language: Language
+    target_language: Language,
+    vocab_list: VocabList | None
 ) -> None:
     """Translates a file and writes the result to another file asynchronously."""
     doc_type = analyze_document_type(source_path)
@@ -29,14 +29,14 @@ async def translate_file_to_file_async(
     try:
         if doc_type == DocumentType.Markdown or doc_type == DocumentType.JupyterNotebook:
             logger.trace("translate in this wat")
-            await translate_notebook_async(root_path, source_path, source_language, target_path, target_language)
+            await translate_notebook_async(root_path, source_path, source_language, target_path, target_language, vocab_list)
         elif doc_type == DocumentType.LaTeX:
             logger.trace("translate latex")
-            await latex_file_translator.translate_file_async(root_path, source_path, source_language, target_path, target_language)
+            await latex_file_translator.translate_file_async(root_path, source_path, source_language, target_path, target_language, vocab_list)
         else:
             # TODO: proper chunking
             # TODO: DB saving
-            translated_content = await translate_file_async(source_path, target_language)
+            translated_content = await translate_file_async(source_path, target_language, vocab_list)
             target_path.parent.mkdir(parents=True, exist_ok=True)
             target_path.write_text(translated_content, encoding="utf-8")
     except IOError as e:
