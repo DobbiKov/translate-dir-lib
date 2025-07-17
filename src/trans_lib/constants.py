c24a104c568c3b16f0a9d0c7d9c74644907d1367
Yehor Korotenko <dobbikov@gmail.com>
2025-07-17 16:00:31 +0200

    changed config dir and files names

--- src/trans_lib/constants.py
+++ src/trans_lib/constants.py
@@ -1,9 +1,9 @@
 
-CONFIG_FILENAME = "trans_conf.json"
+CONFIG_FILENAME = "config.json"
 INTER_FILE_TRANSLATION_DELAY_SECONDS = 5 
 
-DB_DIR_NAME = "trans_git_db"
+DB_DIR_NAME = "translate_db"
 CORRESPONDENCE_DB_NAME = "correspondence_db.csv"
 
-APP_NAME = "trans_git"
+APP_NAME = "translate_dir"
 CONF_DIR = f".{APP_NAME}" 
